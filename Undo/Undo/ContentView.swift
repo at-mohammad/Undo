383845fd946df26b86e5d4f1dd85d4bc8bbd7c3a
AbdelRahman Mohammad <3bdlr7mn@MacBook-37.local>
2025-05-20 11:45:18 +0300

    Initial commit: Set up Xcode project

---
+++ Undo/Undo/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Undo
+//
+//  Created by AbdelRahman Mohammad on 20/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("T")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Undo/Undo/UndoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UndoApp.swift
+//  Undo
+//
+//  Created by AbdelRahman Mohammad on 20/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct UndoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
