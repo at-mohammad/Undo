bbf8488de219f4524e8dbc6d28a61fca714ae00f
Pixel Arabi <PixelArabiOfficial@gmail.com>
2025-06-07 00:23:34 +0300

    minor changes

--- Undo/Undo/Models/AppInfo.swift
+++ Undo/Undo/Models/AppInfo.swift
@@ -9,10 +9,13 @@ import Foundation
 
 struct AppInfo {
     static var version: String {
+        // The application's version number, as defined by the `CFBundleShortVersionString` key in the Info.plist.
+        // - Example: `1.0.2`
         Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "N/A"
     }
     
     static var buildNumber: String {
+        // The application's build number - Example: `345` (Mostly not necessary)
         Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String ?? "N/A"
     }
 }
--- Undo/Undo/Settings/AboutView.swift
+++ Undo/Undo/Settings/AboutView.swift
@@ -16,13 +16,14 @@ struct AboutView: View {
     private let buildNumber = AppInfo.buildNumber
     private let feedbackEmail = "pixelarabiofficial@gmail.com"
     // TODO: Replace with your actual App Id
+    private let githubURL = URL(string: "https://github.com/3BDLR7MN/Undo.git")!
     private let appReviewURL = URL(string: "https://apps.apple.com/app/id1523772947?action=write-review")!
     private let appShareURL = URL(string: "https://apps.apple.com/app/id1523772947")!
-    private let githubURL = URL(string: "https://github.com/3BDLR7MN/Undo.git")!
     private let tiktokURL = URL(string: "https://www.tiktok.com/@pixelarabi")!
     private let youtubeURL = URL(string: "https://www.youtube.com/@pixelarabi")!
     private let instagramURL = URL(string: "https://www.instagram.com/pixelarabi")!
     private let xURL = URL(string: "https://x.com/pixelarabi_")!
+    private let iconURL = URL(string: "https://www.flaticon.com/free-icon/undo_9693669?related_id=9693806&origin=search")!
 
     // MARK: Body
     var body: some View {
@@ -40,12 +41,13 @@ struct AboutView: View {
 //                }
                 
                 Section("Support") {
-                    Link(destination: appReviewURL) {
-                        Label("Leave a Review", systemImage: "star.fill")
-                    }
-                    ShareLink(item: appShareURL) {
-                        Label("Share the App", systemImage: "square.and.arrow.up.fill")
-                    }
+                    // TODO: Uncomment when your own App Id is used
+//                    Link(destination: appReviewURL) {
+//                        Label("Leave a Review", systemImage: "star.fill")
+//                    }
+//                    ShareLink(item: appShareURL) {
+//                        Label("Share the App", systemImage: "square.and.arrow.up.fill")
+//                    }
                     Link(destination: URL(string: "mailto:\(feedbackEmail)?subject=Feedback%20for%20Undo%20App%20(v\(appVersion))")!) {
                         Label("Send Feedback", systemImage: "envelope.fill")
                     }
@@ -63,14 +65,21 @@ struct AboutView: View {
                     Link("X (Twitter)", destination: xURL)
                 }
                 
-                Section("Acknowledgements") {
-                    Text("If Needed")
+                Section("Legal") {
+                    Link("Icon", destination: iconURL)
                 }
                 
-                Section("App Version") {
-                    Text("\(appVersion) (\(buildNumber))")
-                        .foregroundColor(.gray)
+                Section {
+                    VStack {
+                        Text("Version: \(appVersion)")
+                        Text("Created with love by Pixel Arabi") // TODO: Make it better
+                    }
                 }
+                .font(.footnote)
+                .foregroundColor(.gray)
+                .frame(maxWidth: .infinity, alignment: .center)
+                .listRowBackground(Color.clear)
+                
             }
             .navigationTitle("About Undo")
             .foregroundStyle(.black)
